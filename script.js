3b440cb80bcffeb42965603cbac027002e9df048
AleChris <cms.alessandra@gmail.com>
2026-03-27 17:33:20 -0300

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function pedirMusica(nomeMusica) {
-    const seuTelefone = "5511987779277"; // Coloque seu número com DDD aqui (só números)
-    const mensagem = encodeURIComponent(`Oi! Estou no seu show e queria ouvir a música: ${nomeMusica}`);
-    const linkWhatsapp = `https://wa.me/${seuTelefone}?text=${mensagem}`;
-    
-    window.open(linkWhatsapp, '_blank');
-}
+const numero = "5511987779277"; // seu WhatsApp
+
+const musicas = [
+  "10000 Maniacs - Because The Night",
+  "Adele - Rolling In The Deep",
+  "Amy Winehouse - Valerie",
+  "Coldplay - Yellow",
+  "Legião Urbana – Quase Sem Querer"
+];
+
+const container = document.getElementById("list");
+
+musicas.sort().forEach(musica => {
+  const link = document.createElement("a");
+
+  const mensagem = `Chris, pode tocar ${musica}?`;
+  const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+  link.href = url;
+  link.innerText = musica;
+  link.target = "_blank";
+
+  link.style.display = "block";
+  link.style.margin = "10px";
+
+  container.appendChild(link);
+});
